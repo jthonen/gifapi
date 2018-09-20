15c9ad6160a49c9d706729fceca4de0a0502fe89
peak <thonen.jacob@gmail.com>
2018-09-20 10:19:13 -0700

    update

--- assets/app.js
+++ assets/app.js
@@ -3,26 +3,24 @@ $(document).ready(function(){
     var animalarray = ["cat", "dog", "bird", "fish", "crab", "falcon", "bat", "zebra", "horse", "sheep", "cow", "donkey", "rhino"];
     var button = $("<button type='button' class='btn btn-dark'>");
 
-    for ( i = 0; i < animalarray.length; i++ ) {
-        $(".buttons").append("<button type='button' class='btn btn-dark'>" + animalarray[i] + "</button>")
-        console.log(animalarray[i]);
+    buttonrun();
+
+    function buttonrun() {
+        $(".buttons").empty();
+            for ( i = 0; i < animalarray.length; i++ ) {
+                $(".buttons").append("<button type='button' class='btn btn-dark'>" + animalarray[i] + "</button>")
+                console.log(animalarray[i]);
+        };
     };
 
-    $(".submit").on("click", function(){
-        animalarray.push($(this).val());
+    $(".submit").on("click", function() {
+        event.preventDefault();      
+        var value = $(".form-control").val();
+        console.log(value);
+        animalarray.push(value);
+        buttonrun();
+        $(".form-control").val('');
+
     });
-
-   
-    
-
-
-
-
-
-
-
-
-
-
 
 });
