3565fb178791379710099ca6af2c731ba854e322
jthonen <thonen.jacob@gmail.com>
2018-09-22 14:54:52 -0700

    update

--- assets/app.js
+++ assets/app.js
@@ -29,16 +29,12 @@ $(document).ready(function(){
         };
     });
 
-   
-
-  
-
     $(".animal-button").on('click', function(){
 
         var topic = $(this).val();       
         var key = 'kgTj93hZlAWO49nWmnAqGsfHU2TN3xMw';
         var queryurl = "http://api.giphy.com/v1/gifs/search?q=" + topic + "&api_key=" + key + "&limit=10" ;
-
+        
         $.ajax({
             url: queryurl,
             method: "GET"
@@ -46,18 +42,16 @@ $(document).ready(function(){
             console.log(response.data)
             console.log(response.data[0].embed_url);
             var giflist = response.data;
-
+            
             for (var i = 0 ; i < giflist.length ; i++) {
-                console.log(giflist[i].id);
+                console.log(giflist[i].embed_url);
+                console.log(giflist[i].images.fixed_height_still.url);
+
+                var gifurl = giflist[i].images.fixed_height_still.url;
+                var giftag = $("<img src="+gifurl+">");
+                $(".gifs").append(giftag);
 
             }
-            
-            
-            
-           
         })
     })
-    
-
-
 });
